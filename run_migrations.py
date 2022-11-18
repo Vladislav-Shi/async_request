36e5c5c92c8a1eaa8c17cfdbcb61a459418f0ba2
Vladislav-Shi <vlad.shirobockow@yandex.ru>
2022-11-18 13:31:58 +0300

    bd init file

--- run_migrations.py
+++ run_migrations.py
@@ -7,6 +7,7 @@ from settings.config import TORTOISE_ORM
 
 async def main():
     command = Command(tortoise_config=TORTOISE_ORM, app='models')
+    await command.init_db(True)
     await command.init()
     await command.migrate()
 
